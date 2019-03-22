043d4bda739741ee8d641f3b06980e3eaad51736
Shubham <spandey@tcd.ie>
2019-03-22 14:48:42 +0000

    Add Bean Object and Classes

---
+++ Polychrest/src/main/java/griup/polychrest/GenerateRecommendation.java
@@ -0,0 +1,36 @@
+package griup.polychrest;
+
+import java.util.ArrayList;
+
+public class GenerateRecommendation {
+	
+	
+	public ArrayList<User> getAllUser()
+	{
+		return null;
+	}
+	
+	public ArrayList<Shopping> getAll()
+	{
+		return null;
+	}
+	public ArrayList<Shopping> getShoppingByUser(String user)
+	{
+		return null;
+	}
+	
+	public ArrayList<Recommendation> getRecommendationListForUser(String user)
+	{
+		return null;
+	}
+	
+	public ArrayList<Food> getFoodAtShop(String shopName)
+	{
+		return null;
+	}
+	
+	public ArrayList<Shop> getShopThatSellsFood(String foodName)
+	{
+		return null;
+	}
+}
---
+++ Polychrest/src/main/java/griup/polychrest/Recommendation.java
@@ -0,0 +1,31 @@
+package griup.polychrest;
+
+import java.util.ArrayList;
+
+public class Recommendation {
+
+	float hasWeeklyWeightage;
+	float hasByWeeklyWeightage;
+	float hasMonthlyWeightage;
+	public float getHasWeeklyWeightage() {
+		return hasWeeklyWeightage;
+	}
+	public void setHasWeeklyWeightage(float hasWeeklyWeightage) {
+		this.hasWeeklyWeightage = hasWeeklyWeightage;
+	}
+	public float getHasByWeeklyWeightage() {
+		return hasByWeeklyWeightage;
+	}
+	public void setHasByWeeklyWeightage(float hasByWeeklyWeightage) {
+		this.hasByWeeklyWeightage = hasByWeeklyWeightage;
+	}
+	public float getHasMonthlyWeightage() {
+		return hasMonthlyWeightage;
+	}
+	public void setHasMonthlyWeightage(float hasMonthlyWeightage) {
+		this.hasMonthlyWeightage = hasMonthlyWeightage;
+	}
+	
+	
+	
+}
